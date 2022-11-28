397706c5d119096b54e7da2ecec5370354e2143f
A M <avikmukherjee88@gmail.com>
2022-11-28 15:56:56 +0530

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD.Models
+{
+    public class Category
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Key]
+        public string? EmpId { get; set; }
+        [Required]
+        public string? Designation { get; set; }
+        [Required]
+        public string? BaseDU { get; set; }
+
+    }
+}
